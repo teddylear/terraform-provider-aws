44ac61abe3965d24df3250ccb688b6039482bdcc
teddylear <20077627+teddylear@users.noreply.github.com>
2024-01-18 17:51:45 -0500

    work on this branch

--- internal/service/iam/service_package_gen.go
+++ internal/service/iam/service_package_gen.go
@@ -20,7 +20,11 @@ func (p *servicePackage) FrameworkDataSources(ctx context.Context) []*types.Serv
 }
 
 func (p *servicePackage) FrameworkResources(ctx context.Context) []*types.ServicePackageFrameworkResource {
-	return []*types.ServicePackageFrameworkResource{}
+	return []*types.ServicePackageFrameworkResource{
+		{
+			Factory: newResourceRole,
+		},
+	}
 }
 
 func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePackageSDKDataSource {
